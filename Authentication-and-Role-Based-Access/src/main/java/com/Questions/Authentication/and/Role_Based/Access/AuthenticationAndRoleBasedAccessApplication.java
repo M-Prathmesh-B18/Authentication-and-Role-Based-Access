1523f8dcabb8b716491f2070791a3255116727b8
M Prathmesh <prathameshmahangare369@gmail.com>
2026-04-09 19:24:49 +0530

    JWT project

---
+++ Authentication-and-Role-Based-Access/src/main/java/com/Questions/Authentication/and/Role_Based/Access/AuthenticationAndRoleBasedAccessApplication.java
@@ -0,0 +1,13 @@
+package com.Questions.Authentication.and.Role_Based.Access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAndRoleBasedAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAndRoleBasedAccessApplication.class, args);
+	}
+
+}
---
+++ Authentication-and-Role-Based-Access/src/test/java/com/Questions/Authentication/and/Role_Based/Access/AuthenticationAndRoleBasedAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Questions.Authentication.and.Role_Based.Access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAndRoleBasedAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
